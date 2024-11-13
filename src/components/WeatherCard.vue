2991b6baf38d8c798643117b8bcb3927a3630595
hanane987 <aminahanane987@gmail.com>
2024-11-13 20:11:24 +0100

    Feat(MW-36): Ajoutez le fichier WeatherCard.vue c'est pour pour afficher les données météo

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="weather-card">
+    <h2>{{ weather.name }}</h2>
+    <p>{{ weather.weather[0].description }}</p>
+    <p>Temperature: {{ weather.main.temp }}°C</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Weather } from '../types/weather';
+
+export default defineComponent({
+  name: 'WeatherCard',
+  props: {
+    weather: {
+      type: Object as PropType<Weather>,
+      required: true,
+    },
+  },
+});
+</script>
+
