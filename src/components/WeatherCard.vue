4bc2a0f4ee1061e814cff0fd108095d295128c62
hanane987 <aminahanane987@gmail.com>
2024-11-14 10:10:36 +0100

    Docs(MW-36): Modifiez les fichies pour afficher le forcast du 7 jours et chaque jour avec tout les heurs

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,13 +1,4 @@
-<template>
-  <div v-if="weather && weather.location && weather.current" class="weather-card">
-    <h2>{{ weather.location.name }}</h2>
-    <p>{{ weather.current.condition?.text || 'Condition data not available' }}</p>
-    <p>Temperature: {{ weather.current.temp_c }}°C</p>
-  </div>
-  <div v-else>
-    <p>Loading weather data...</p>
-  </div>
-</template>
+
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
