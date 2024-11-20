c6ac9da8e9c2b971b4ee3686f58d53bfe5ac52db
hanane987 <aminahanane987@gmail.com>
2024-11-20 14:39:06 +0100

    Feat(MW-36):add the files for the translation in the folder i18n

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,28 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import { createI18n } from 'vue-i18n'; // Import vue-i18n
+
+// Import translation files
+import en from './i18n/en.json';
+import fr from './i18n/fr.json';
+import ar from './i18n/ar.json';
 
 import './assets/styles.css'; 
 
-
+// Set up vue-i18n
+const i18n = createI18n({
+  locale: 'en',  
+  messages: {
+    en,
+    fr,
+    ar
+  }
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(i18n); 
 app.mount('#app');
