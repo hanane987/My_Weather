983d2fdf43bc0d2d34e2db13c6a363dca1295bf4
hanane987 <aminahanane987@gmail.com>
2024-11-20 15:58:11 +0100

    Docs(MW-36):Update and complete the weatherService.ts in to get the daily and weekly forecast weather

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -12,3 +12,29 @@ export async function fetchWeatherData(city: string): Promise<WeatherData> {
   }
 
   const data = await response.json();
+  return {
+    current: {
+      temp: data.current.temp_c,
+      feelsLike: data.current.feelslike_c,
+      condition: data.current.condition.text,
+      icon: data.current.condition.icon,
+      humidity: data.current.humidity,
+      windSpeed: data.current.wind_kph,
+      precipitation: data.current.precip_mm,
+      aqi: data.current.air_quality['us-epa-index']
+    },
+    hourly: data.forecast.forecastday[0].hour.map((hour: any) => ({
+      time: hour.time,
+      temp: hour.temp_c,
+      icon: hour.condition.icon,
+      condition: hour.condition.text
+    })),
+    daily: data.forecast.forecastday.map((day: any) => ({
+      date: day.date,
+      high: day.day.maxtemp_c,
+      low: day.day.mintemp_c,
+      icon: day.day.condition.icon,
+      condition: day.day.condition.text
+    }))
+  };
+} 
